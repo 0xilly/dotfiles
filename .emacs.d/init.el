d01b59da29f6c0d954d6acd5e731c748701eb2a4
Anthony Anderson <aantony4122@gmail.com>
2022-12-12 22:30:09 -0500

    Added base emacs files
    Signed-off-by: Anthony Anderson <aantony4122@gmail.com>

---
+++ .emacs.d/init.el
@@ -0,0 +1,95 @@
+;; Add Melpa packages to Repos
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(use-package emacs
+  :preface
+  (defvar ian/indent-width 2) ; change this value to your preferred width
+  :config
+  (setq frame-title-format '("Anita") 
+        ring-bell-function 'ignore       
+        frame-resize-pixelwise t
+        default-directory "~/")
+
+  (tool-bar-mode -1)
+  ;(menu-bar-mode -1)
+
+  ;; better scrolling experience
+  (setq scroll-margin 0
+        scroll-conservatively 101 ; > 100
+        scroll-preserve-screen-position t
+        auto-window-vscroll nil)
+
+  ;; Always use spaces for indentation
+  (setq-default indent-tabs-mode nil
+                tab-width ian/indent-width)
+
+  ;; Omit default startup screen
+  (setq inhibit-startup-screen t))
+
+;; The Emacs default split doesn't seem too intuitive for most users.
+(use-package emacs
+  :ensure nil
+  :preface
+  (defun ian/split-and-follow-horizontally ()
+    "Split window below."
+    (interactive)
+    (split-window-below)
+    (other-window 1))
+  (defun ian/split-and-follow-vertically ()
+    "Split window right."
+    (interactive)
+    (split-window-right)
+    (other-window 1))
+  :config
+  (global-set-key (kbd "C-x 2") #'ian/split-and-follow-horizontally)
+  (global-set-key (kbd "C-x 3") #'ian/split-and-follow-vertically))
+
+
+;; Evil Mode
+(use-package evil
+  :ensure t
+  :config
+
+  (evil-mode 1)
+  (use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode t)
+    (evil-leader/set-leader "<SPC>")
+    (evil-leader/set-key
+      "s s" 'swiper
+      "d x w" 'delete-trailing-whitespace))
+
+  (use-package evil-surround
+    :ensure t
+    :config (global-evil-surround-mode))
+
+  (use-package evil-indent-textobject
+    :ensure t)
+
+  (use-package evil-org
+    :ensure t
+    :config
+    (evil-org-set-key-theme
+	  '(textobjects insert navigation additional shift todo heading))
+    (add-hook 'org-mode-hook (lambda () (evil-org-mode))))
+
+  (use-package powerline-evil
+    :ensure t
+    :config
+    (powerline-evil-vim-color-theme)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(powerline-evil evil-org evil-indent-textobject evil-surround evil-leader evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
