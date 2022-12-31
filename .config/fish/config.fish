5b33d9318b6c49d9cfcaa2b78f6237633cb5b11c
Anthony Anderson <aantony4122@gmail.com>
2022-12-31 03:29:00 -0500

    fix llvm path for fish
    Signed-off-by: Anthony Anderson <aantony4122@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,6 @@ end
 alias v="lvim"
 alias vim="lvim"
 
-
 alias v="nvim"
 alias vim="nvim"
 
@@ -22,11 +21,14 @@ switch (uname)
     set -x PATH /Applications/CMake.app/Contents/bin $PATH
     set -x PATH /Users/aa/.local/bin $PATH
     set -x PATH /Users/aa/Library/Python/3.10/bin $PATH
+
+    set LLVM_BIN /opt/homebrew/Cellar/llvm/14.0.6_1/bin
+    set -g -x CC $LLVM_BIN/clang
+    set -g -x CXX $LLVM_BIN/clang++
+
+    set -x PATH $HOME/Devel/dartshit/fluttersdk/flutter/bin $PATH
   case Linux
   case '*'
 
 end
 
-#set LLVM_BIN /opt/homebrew/Cellar/llvm/14.0.6_1/bin
-#set -g -x CC $LLVM_BIN/clang
-#set -g -x CXX $LLVM_BIN/clang++
