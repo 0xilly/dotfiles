4d3e288ab6201fe9458241cdf1d1db3ef7bddb7f
Anita Anderson <anita@ansan.io>
2023-11-16 09:52:11 -0500

    Move config to lazy
    Signed-off-by: Anita Anderson <anita@ansan.io>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,2 @@
-local g = vim.g
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.g.editorconfig = true
-
-require('utils')
-require('settings')
-require('packer_boot')
-require('pkgs')
-
---map('n', '<leader>')
---m
-
-
-map("n", "<leader>om",":lua print('foo')<CR> ") -- not working
---require('keymaps')
-
--- auto-reload files when modified externally
--- https://unix.stackexchange.com/a/383044
-
-
-
---vim.cmd.colorscheme 'melange'
+require('anita.core')
+require('anita.lazy')
---
+++ .config/nvim/lua/anita/core/options.lua
@@ -0,0 +1,44 @@
+local opt = vim.opt
+
+--line numbers
+opt.relativenumber = true 
+opt.number = true 
+
+-- tabs & indentation
+opt.tabstop = 2 
+opt.shiftwidth = 2 
+opt.expandtab = false
+opt.autoindent = false
+
+vim.g.editorconfig = false
+
+-- line wrapping
+opt.wrap = false 
+
+-- search settings
+opt.ignorecase = true 
+opt.smartcase = true 
+
+-- cursor line
+opt.cursorline = true 
+
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
